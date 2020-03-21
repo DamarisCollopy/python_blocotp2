f05d0fc1ab61ebe4bbeb968727acd6e2cb417a51
Damaris-PC <damisoliveira@gmail.com>
2020-03-21 18:14:30 -0300

    Trabalho 3 projeto de bloco

--- grafico_cpu.py
+++ grafico_cpu.py
@@ -2,7 +2,8 @@ import psutil
 import platform
 import time
 from matplotlib import pyplot as plt
-
+import cpuinfo
+from cpuinfo import get_cpu_info
 
 # fiz um switch para gerenciar melhor os pedidos
 def meu_switch():
@@ -14,7 +15,10 @@ def meu_switch():
                       "\n 1 : Uso CPU "
                       "\n 2 : Uso Disco"
                       "\n 3 : Ip Maquina"
-                      "\n 4 : Sair \n"))
+                      "\n 4 : Processador"
+                      "\n 5 : Arquitetura de Rede"
+                      "\n 6 : Informacao CPUs"
+                      "\n 7 : Sair \n"))
         if z < x:
             uso_memoria()
         elif z == x:
@@ -22,8 +26,15 @@ def meu_switch():
         elif z == 2:
             uso_disco()
         elif z == 3:
-            ip_maquina()
+            detalhes_rede()
         elif z == 4:
+            processador()
+        elif z == 5:
+            arquitetura_info()
+        elif z == 6:
+            quantas_cpus()
+            exibir_percent_cpu_grafico(10)
+        elif z == 7:
             print("Programa encerrado")
             break
         else:
@@ -41,6 +52,7 @@ def uso_memoria():
     #O programa acima irá produzir informação de uso de processamento a
     # cada segundo, 100 vezes. O comando time.sleep(1) serve para esperar 1 segundo até a próxima leitura.
     # plot mostra essa sequencia no grafico
+
 def uso_cpu():
     lista_cpu_percent = []
 
@@ -67,10 +79,57 @@ def uso_disco():
     plt.title = 'Uso do disco '
     plt.show()
 
+# uname retorna uma tupla de strings (sistema,nome adm,  versão, máquina, processador)
+# processor identifica o nome do processador .
+#num_threads retorne os threads abertos pelo processo como uma lista de tuplas nomeadas,
+# incluindo ID do thread e tempos de CPU do thread (usuário / sistema
+# usei a documentacao como guia
+# https://www.google.com/search?q=tradutot&oq=tradutot&aqs=chrome..69i57j0l7.3520j0j7&sourceid=chrome&ie=UTF-8
+def processador() :
+    print(platform.processor())
+    print(platform.uname())
+    p = psutil.Process()
+    print("Numero de Threads" + "" + str(p.num_threads()))
+    
 
-def ip_maquina():
-    dic_interfaces = psutil.net_if_addrs()
-    print(dic_interfaces['Ethernet'][1][1])
+def detalhes_rede() :
+    detalhes_rede = psutil.net_if_addrs()
+    num_interfaces = len(detalhes_rede)
+    for i in range(num_interfaces):
+        print(detalhes_rede['Ethernet'][i][1])
+
+#Informação de arquitetura
+def arquitetura_info() :
+
+    cpu = cpuinfo.get_cpu_info()
+    print(cpu['arch'])
+    print(cpu['brand'])
+    print(cpu['bits'])
+    print(cpu['count'])
+    print(cpu['hz_actual'])
+
+def exibir_grafico(valores) :
+    for lista in valores :
+        plt.plot(lista)
+
+def quantas_cpus() :
+
+    for t in range(10) :
+        print(psutil.cpu_percent(interval=0.1 , percpu = True))
+
+#Exibe um grafico dos nucleos e threads
+def exibir_percent_cpu_grafico(segundos) :
+    num_cores = psutil.cpu_count()
+    lista_uso_cores = []
+
+    for i in range(num_cores) :
+        lista_uso_cores.append([])
+
+    for t in range(segundos) :
+        percent = psutil.cpu_percent(interval=1, percpu=True)
+        for i in range(num_cores):
+            lista_uso_cores[i].append(percent[i])
+    exibir_grafico(lista_uso_cores)
 
 
 if __name__ == "__main__":
